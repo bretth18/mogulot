34e2175e0de6a71d4e41f25363dc700543e76023
Brett Henderson <bretth18@gmail.com>
2015-12-07 11:31:34 -0800

    added framework for binaryServer
    obviously needs work

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 //index.js
 //2015 bretth18
 
+//+++++++++++++++++++++++++++++++++++++++++++++
+//TODO: create functional audioservice handler
+//      lossless audioservice, audioAPI
+//+++++++++++++++++++++++++++++++++++++++++++++
+
 
 var http = require('http');
 var app = require('express')();
@@ -9,3 +14,45 @@ var app = require('express')();
 var server = http.createServer(app).listen(9000);
 
 // attach BinaryServer to the base http server
+var BinaryServer = require('/node_modules/binaryjs/').BinaryServer;
+var bs = BinaryServer({server: server});
+
+//express routing
+
+// Wait for new user connections
+bs.on('connection', function(client){
+  console.log('Client has connected to remote server');
+  client.on('connection', function(broadcast, client){
+    username: client.username
+    message: client.data
+  }
+
+  // Incoming stream from browsers
+  client.on('stream', function(stream, meta){
+    //
+    var file = fs.createWriteStream(__dirname+ '/public/' + meta.name);
+    stream.pipe(file);
+    //
+
+    //Audio stream handler
+    //Audioservice is pulled from server based audio input
+
+    var audioservice =  require('/lib/audioAPI').audioservice;
+      stream.pipe('##REMOTEUPSTREAMAUDIO');
+      if client.on('stream', null){
+        console.log('Client failed to connect to remote upstream audio');
+
+
+      }
+    // Send progress back
+    stream.on('data', function(data){
+      stream.write({rx: data.length / meta.size});
+    });
+    //
+  });
+});
+//
+//
+
+server.listen(9000);
+console.log('HTTP and BinaryJS server started on port 9000');
